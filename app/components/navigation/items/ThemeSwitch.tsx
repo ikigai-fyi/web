28a1ece3f395d55577f0461a4ba3308f3da25fcd
Paul Nicolet <nicoletpaul@orange.fr>
2023-12-21 17:13:13 +0100

    Style theme switch button

--- app/components/navigation/items/ThemeSwitch.tsx
+++ app/components/navigation/items/ThemeSwitch.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import styles from "./styles.module.css";
 import { useTheme } from "next-themes";
 
@@ -7,11 +8,16 @@ export default function ThemeSwitch() {
   const { theme, setTheme } = useTheme();
   return (
     <button
-      className={styles.navItem}
+      className={`${styles.navItem} ${styles.themeSwitch}`}
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
       suppressHydrationWarning
     >
-      {theme === "light" ? "Light" : "Dark"}
+      <Image
+        src={theme === "light" ? "/moon.svg" : "/sun.svg"}
+        width={16}
+        height={16}
+        alt={theme === "light" ? "moon" : "sun"}
+      />
     </button>
   );
 }
