b800807a973fdf965118f3a7bc79b39678d61236
Paul Nicolet <nicoletpaul@orange.fr>
2023-12-21 18:30:00 +0100

    Plug dark mode

--- app/components/Navbar/items/ThemeSwitch.tsx
+++ app/components/Navbar/items/ThemeSwitch.tsx
@@ -2,16 +2,29 @@
 
 import Image from "next/image";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 import styles from "./styles.module.css";
 
 export default function ThemeSwitch() {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <button
-      className={`${styles.navItem} ${styles.themeSwitch}`}
+      className={`${styles.navItem} ${styles.themeSwitch} ${
+        theme === "light" ? styles.themeSwitchLight : styles.themeSwitchDark
+      }`}
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      suppressHydrationWarning
     >
       <Image
         src={theme === "light" ? "/moon.svg" : "/sun.svg"}
