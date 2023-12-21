22dedfd0c3c45ea8a21c970e164c50128e085c17
Paul Nicolet <nicoletpaul@orange.fr>
2023-12-21 17:24:04 +0100

    Fix casing

---
+++ app/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import styles from "./Footer.module.css";
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className={styles.container}>
+      <Link href="/" className={styles.footer}>
+        © {currentYear} Ikigai
+      </Link>
+    </footer>
+  );
+}
